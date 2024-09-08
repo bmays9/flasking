41e96038034d1d1e5a7fad4f952457cbd7abf7f4
Barry Mays <barrykmays@gmail.com>
2024-09-08 11:16:00 +0000

    Initial run.py

---
+++ run.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask //import Flask Class
+
+
+app = Flask(__name__) // using the class, calling it app. 
+
+
+@app.route("/")
+def index():
+    return "Hello, World"
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
